d48ef7fdb67c85b9dc4dc5d69f44481e15fd105c
Molly Graham <mollyclaire.graham@gmail.com>
2019-07-30 14:48:11 -0500

    Updates

---
+++ precache-manifest.a182786f836a9671520dc5d841924fa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d42d7264035298b33c45ec33deba5d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b9a1d2bc90c32c776eb6",
+    "url": "/clicky-game/static/css/main.d6d1f7fc.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b9a1d2bc90c32c776eb6",
+    "url": "/clicky-game/static/js/main.41fc4c62.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
